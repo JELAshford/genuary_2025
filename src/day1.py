760448f98c49e7557322933502ec26e1ffc02026
James Ashford <ashfordj@rocketmail.com>
2025-01-01 21:32:14 +0000

    Added a colour version

--- src/day1.py
+++ src/day1.py
@@ -1,6 +1,7 @@
 """Horizontal and Vertical Lines"""
 
 import matplotlib.pylab as plt
+from einops import rearrange
 from typing import Tuple
 import numpy as np
 
@@ -23,21 +24,46 @@ def add_lines(
         array[start_pos : (start_pos + length), (x_pos - thickness) : (x_pos)] = (
             rng.random()
         )
-    return image
+    return array
+
+
+def generate_gray_image(rng_obj: np.random.Generator):
+    array = np.ones(shape=(SIZE, SIZE))
+    array = add_lines(array, (20, 40), SIZE // 40, rng_obj=rng_obj)
+    array = add_lines(array, (4, 10), SIZE // 20, rng_obj=rng_obj)
+    array = add_lines(array, (1, 3), SIZE // 10, rng_obj=rng_obj)
+    return array
+
+
+def repeat_channels(array: np.array, offset: int = 100):
+    array_red = array.copy()
+    array_green = np.roll(np.roll(array.copy(), offset, axis=0), offset, axis=1)
+    array_blue = np.roll(np.roll(array.copy(), -offset, axis=0), -offset, axis=1)
+    array = rearrange([array_red, array_green, array_blue], "b h w -> h w b")
+    return array
 
 
 SIZE = 500
-rng = np.random.default_rng(1701)
 
+rng = np.random.default_rng(1701)
 fig, axs = plt.subplots(1, 1, figsize=(10, 10), squeeze=False)
 for ax in axs.flatten():
     axis_rng = np.random.default_rng(rng.integers(1, 1e6, size=1)[0])
-    image = np.ones(shape=(SIZE, SIZE))
-    image = add_lines(image, (20, 40), SIZE // 40, rng_obj=axis_rng)
-    image = add_lines(image, (4, 10), SIZE // 20, rng_obj=axis_rng)
-    image = add_lines(image, (1, 3), SIZE // 10, rng_obj=axis_rng)
+    image = generate_gray_image(axis_rng)
     ax.imshow(image, cmap="gray")
     ax.set_xticks([])
     ax.set_yticks([])
 plt.savefig("out/day1.png")
+plt.show()
+
+rng = np.random.default_rng(1701)
+fig, axs = plt.subplots(1, 1, figsize=(10, 10), squeeze=False)
+for ax in axs.flatten():
+    axis_rng = np.random.default_rng(rng.integers(1, 1e6, size=1)[0])
+    image = generate_gray_image(axis_rng)
+    image = repeat_channels(image, offset=5)
+    ax.imshow(image, cmap="gray")
+    ax.set_xticks([])
+    ax.set_yticks([])
+plt.savefig("out/day1_chrom.png")
 plt.show()
