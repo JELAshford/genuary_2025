01e1b837c1da7bcd78af087995f51c63e6e85a35
James Ashford <ashfordj@rocketmail.com>
2025-01-02 19:51:20 +0000

    Day 2 and some new day1 examples

---
+++ src/day2.py
@@ -0,0 +1,53 @@
+"""Layers"""
+
+import matplotlib.pylab as plt
+from einops import rearrange
+import numpy as np
+
+SIZE = 1000
+grid = np.ones(shape=(SIZE, SIZE))
+rng = np.random.default_rng(1701)
+
+# Create distance array for drawing circles
+x, y = np.indices(grid.shape)
+distance_array = np.sqrt((x - SIZE // 2) ** 2 + (y - SIZE // 2) ** 2)
+
+# Draw random circles
+for y, x in rng.integers(10, SIZE - 10, size=(100, 2)):
+    circle_mask = distance_array < rng.integers(20, 100, size=1)[0]
+    circle_mask = np.roll(
+        np.roll(circle_mask, y - SIZE // 2, axis=0), x - SIZE // 2, axis=1
+    )
+    grid[circle_mask] = rng.random()
+
+# Visualise colours
+offset = 10
+grid_red = grid.copy()
+grid_green = np.roll(np.roll(grid.copy(), offset, axis=0), offset, axis=1)
+grid_blue = np.roll(np.roll(grid.copy(), -offset, axis=0), -offset, axis=1)
+grid = rearrange([grid_red, grid_green, grid_blue], "b h w -> h w b")
+
+mid = SIZE // 2
+quart = SIZE // 4
+corner = grid[:mid, :mid]
+grid[:mid, mid:] *= 1 / corner
+grid[mid:, :mid] *= corner
+grid[mid:, mid:] *= 1 / corner
+
+shade_size = 5
+for inset in np.linspace(0, 0.5, 15):
+    dist = int(inset * SIZE)
+    back_dist = SIZE - dist
+    new_section = np.rot90(grid[dist:back_dist, dist:back_dist].copy())
+    grid[
+        (dist - 1) : (back_dist + shade_size),
+        (dist - 1) : (back_dist + shade_size),
+    ] *= 0.8
+    grid[dist:back_dist, dist:back_dist] = new_section
+
+fig, ax = plt.subplots(1, 1, figsize=(10, 10))
+ax.imshow(grid, cmap="gray")
+ax.set_xticks([])
+ax.set_yticks([])
+plt.savefig("out/day2.png")
+plt.show()
